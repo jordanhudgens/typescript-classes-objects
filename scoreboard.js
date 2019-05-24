31618bcd0cb202d83e9a30a0c9d28851d74716e2
jordanhudgens <jordan@devcamp.com>
2019-05-23 18:08:35 -0700

    01 - objects and classes introduction

---
+++ scoreboard.js
@@ -0,0 +1,10 @@
+var Team = /** @class */ (function () {
+    function Team() {
+    }
+    Team.prototype.generateLineup = function () {
+        return "Lineup goes here...";
+    };
+    return Team;
+}());
+var astros = new Team();
+astros.generateLineup(); //?
---
+++ scoreboard.ts
@@ -0,0 +1,8 @@
+class Team {
+  generateLineup() {
+    return "Lineup goes here...";
+  }
+}
+
+const astros = new Team();
+astros.generateLineup(); //?
