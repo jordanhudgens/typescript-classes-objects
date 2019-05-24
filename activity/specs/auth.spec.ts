7a8dc6a175a46af0d4abea8d69fdf02f46213c0d
jordanhudgens <jordan@devcamp.com>
2019-05-24 12:49:14 -0700

    Built out working solution for the activity

---
+++ activity/specs/auth.spec.ts
@@ -0,0 +1,9 @@
+import Auth from "../auth.js";
+import "jasmine";
+
+describe("heat map data shape", () => {
+  it("should have an attribute called coldZones", () => {
+    const x = Auth;
+    expect(true).toBe(true);
+  });
+});
