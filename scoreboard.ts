2e817bdf1bb8e6404843f1b02bd92491ebaf8288
jordanhudgens <jordan@devcamp.com>
2019-06-10 14:16:17 -0600

    Step 14

--- scoreboard.ts
+++ scoreboard.ts
@@ -18,11 +18,15 @@ class Team {
 }
 
 const astrosPlayers = ["Altuve", "Bregman", "Correa", "Springer"];
-const astros = new Team("Astros", astrosPlayers);
-astros.generateLineup(); //?
-astros.name; //?
+const astros = new Team({
+  name: "Astros",
+  players: astrosPlayers
+});
 
-const blueJaysPlayers = ["Vlad", "Smoak", "Tellez", "Sogard"];
-const blueJays = new Team("Blue Jays", blueJaysPlayers);
-blueJays.generateLineup(); //?
-blueJays.name; //?
+// astros.generateLineup(); //?
+// astros.name; //?
+
+// const blueJaysPlayers = ["Vlad", "Smoak", "Tellez", "Sogard"];
+// const blueJays = new Team("Blue Jays", blueJaysPlayers);
+// blueJays.generateLineup(); //?
+// blueJays.name; //?
